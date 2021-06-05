3308a3619201b6a77d3cfcacc5b3e9c38ab7cc98
Cheetohzzz <50166552+Cheeetohz@users.noreply.github.com>
2021-06-05 15:25:17 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Why Dogecoin ?</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Trust In Doge</h1>
+      <p>
+       Although We Had A minor setback in may Dogecoin will rise and we're pushing it to the moon. Buy The Dip.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
